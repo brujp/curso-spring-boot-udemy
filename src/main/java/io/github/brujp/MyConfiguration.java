a991ff53de847851b3c6585fd5d7e9f6ca8f7218
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-12-28 19:45:23 -0300

    Trabalhando com multiplos ambientes e profiles

--- src/main/java/io/github/brujp/MyConfiguration.java
+++ src/main/java/io/github/brujp/MyConfiguration.java
@@ -1,18 +1,23 @@
 package io.github.brujp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 //Configurations e Beans
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
     //Aqui eu configuro a criação do objeto no contexto da aplicação
     // pra utilizar onde eu precisar
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas 2";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DEV"); //Meu profile está apontando para dev.
+        };
     }
 
 }
